1c24b473a97ffe0989785cd2de4ab2812bde5735
kryczek <kryczuss@gmail.com>
2017-05-27 10:05:55 +0200

    add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :autheticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :aunanthenticated_root
+    end
+  end
+  
 end
